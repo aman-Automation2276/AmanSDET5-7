b8d3d89e434d58c355e9f986aaee8640c1b46c42
aman-Automation2276 <aman2276@gmail.com>
2022-05-07 19:46:50 +0530

    Aman new commit for jenkins

---
+++ com.DhProject/src/test/java/CreateContactNew.java
@@ -0,0 +1,11 @@
+import org.testng.annotations.Test;
+
+public class CreateContactNew {
+
+	@Test
+	public void createContact()
+	{
+		System.out.println("Contact created");
+		System.out.println("Contact created for jenkins");
+	}
+}
