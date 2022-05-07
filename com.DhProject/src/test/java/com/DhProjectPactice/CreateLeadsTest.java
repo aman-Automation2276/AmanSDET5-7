ca3e18f0a03c6d3b993c8aaa54ccde93ce39bb10
aman-Automation2276 <aman2276@gmail.com>
2022-05-07 10:38:17 +0530

    1st commit

---
+++ com.DhProject/src/test/java/com/DhProjectPactice/CreateLeadsTest.java
@@ -0,0 +1,18 @@
+package com.DhProjectPactice;
+
+import org.testng.annotations.Test;
+
+public class CreateLeadsTest {
+
+	@Test
+	public void createTest()
+	{
+		System.out.println("chnges");
+		System.out.println("Leads created");
+		
+		
+		
+		
+		
+	}
+}
